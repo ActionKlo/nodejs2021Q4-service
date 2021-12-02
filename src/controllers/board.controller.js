17534a74978cd0b502adcd1fe36f49ce511505cc
ActionKlo <actionclaw@gmail.com>
2021-12-02 15:57:46 +0100

    fix: board methods

--- src/controllers/board.controller.js
+++ src/controllers/board.controller.js
@@ -1,15 +1,25 @@
 const uuid = require('uuid')
-const board = require('../db/board.db')
+const boards = require('../db/board.db')
+const tasks = require('../db/tasks.db')
 
 exports.getAllBoards = (_, reply) => {
-	reply.status(200).send(board.getAll())
+	reply.status(200).send(boards.getAll())
+}
+
+exports.getBoardById = (request, reply) => {
+	const board = boards.getById(request.params.boardId)
+
+	if (!board) {
+		reply.status(404).send({ error: "Board not found" })
+	}
+
+	reply.status(200).send(board)
 }
 
 exports.createBoard = (request, reply) => {
 	const data = {}
 	data.title = request.body.title
 
-	console.log(request.body)
 	if (request.body.columns) {
 		const columns = request.body.columns.map(column => ({
 				id: uuid.v4(),
@@ -20,6 +30,22 @@ exports.createBoard = (request, reply) => {
 		data.columns = columns
 	}
 
-	console.log(data)
-	reply.status(201).send(board.create(data))
+	reply.status(201).send(boards.create(data))
+}
+
+exports.putBoard = (request, reply) => {
+	const data = request.body
+	const {boardId} = request.params
+
+	reply.status(200).send(boards.put(boardId, data))
+}
+
+exports.deleteBoardById = (request, reply) => {
+	const { boardId } = request.params
+	
+	if (boards.delete(boardId)) {
+		tasks.deleteTaskByBordId(boardId)
+		reply.status(200).send({ msg: "Board deleted"})
+	}
+	reply.status(404).send({ err: "Board not found"})
 }
